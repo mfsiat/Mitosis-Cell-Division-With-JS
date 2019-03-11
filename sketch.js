b51b993dddb9cbb717a3ea7fce59adb1deeaf599
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-03-12 01:11:37 +0600

    Final Commit after the build

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var cells = [];
+function setup() {
+  createCanvas(700, 700);
+  cells.push(new Cell());
+  cells.push(new Cell());
+}
+
+function draw() {
+  background(200);
+  for(var i = 0; i < cells.length; i++){
+    cells[i].move();
+    cells[i].show();
+    
+  }
+}
+
+
+function mousePressed(){
+    for(var i = cells.length-1; i >= 0; i--){
+      if(cells[i].clicked(mouseX, mouseY)){
+        //var cellB = cells[i].mitosis();
+        cells.push(cells[i].mitosis());
+        cells.push(cells[i].mitosis());
+        //console.log("CLICKED");
+        cells.splice(i, 1);
+      }
+    }
+
+}
